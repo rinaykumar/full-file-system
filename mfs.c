e24abc5006c0d51a211282aa94d8e8fdb25c05c4
rinaykumar <rkumar4@mail.sfsu.edu>
2020-11-30 21:32:17 -0800

    Attempts at some of the directory functions in mfs.c

--- mfs.c
+++ mfs.c
@@ -27,36 +27,50 @@
 
 int fs_mkdir(const char *pathname, mode_t mode)
 {
-    // If using inodes as directories?
+    // If using inodes as directories:
     createInode(I_DIR, pathname);
 
+    // Alternative way to create directories:
     // char newdir[] = pathname;
     // char path1[MAX_FILEPATH_SIZE], path2[MAX_FILEPATH_SIZE], *p;
-
     // mknod(newdir, S_IFDIR|mode);
-
     // strcpy(path1, newdir);
     // strcat(path1, "/.");
     // link(newdir, path1);
     // strcat(path1, ".");
     // strcpy(path2, newdir);
-
     // if ((p = strrchr(path2, '/')) == (char *)0) {
     //     link(".", path1);
     // } else {
     //     *p = '\0';
     //     link(path2, path1);
     // }
+
+    return 0;
 }
 
 int fs_rmdir(const char *pathname)
 {
+    // Assuming inodes are used as directories
 
+    // Get the inode
+    fs_dir* inodeToRemove = getInode(pathname);
+
+    // Remove inode from parent
+    removeFromParent(inodeToRemove->parent, inodeToRemove); // Need to access inode's parent and pass into function
+
+    // Free inode
+    freeInode(inodeToRemove);
+
+    return 0;
 }
 
 fs_dir* fs_opendir(const char *name) 
 {
+    // Call getInode 
+    fs_dir* openedDir = getInode(name);
 
+    return openedDir;
 }
 
 struct fs_dirEntry *fs_readdir(fs_dir *dirp) 
@@ -83,12 +97,30 @@ int fs_setcwd(char *buf)
 // Return 1 if file, 0 otherwise
 int fs_isFile(char * path)
 {
+    // Get the inode from path
+    fs_dir* inode = getInode(path);
+
+    // Check inode type
+    if (inode->type == I_FILE) {
+        return 1;
+    } else {
+        return 0;
+    }
 
 }
 
 // Return 1 if directory, 0 otherwise
 int fs_isDir(char * path) 
 {
+    // Get the inode from path
+    fs_dir* inode = getInode(path);
+
+    // Check inode type
+    if (inode->type == I_DIR) {
+        return 1;
+    } else {
+        return 0;
+    }
 
 }
 
@@ -113,6 +145,7 @@ int fs_stat(const char *path, struct fs_stat *buf)
 
 }
 
+// Not sure if this is supposed to be here, but it was in mfs.h 
 char* getInodeTypeName(char* buf, InodeType type) 
 {
 
